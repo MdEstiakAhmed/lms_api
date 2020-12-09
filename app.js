c3f0e9f370e99482c9bd1700c4c752a59ddcc60c
MdEstiakAhmed <estiak97@gmail.com>
2020-12-10 00:19:39 +0600

    aa

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('*', (req, res) => {
 // route
 
 // server create
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
     connectDatabase();
